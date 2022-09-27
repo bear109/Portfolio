b020998ad5fb79d7e3cf57f3d622dcdb6c95586b
bear109 <jokerbear109@gmail.com>
2022-09-27 14:41:39 +0900

    feat: Navbar, Footer UI 적용

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import tw from 'twin.macro'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="text-gray-600 body-font">
+      <div css={style.footerWrapper}>
+        <span css={style.footerMainText}>bear109&apos;s portfolio</span>
+        <p css={style.copyrightText}>© 2022 Seo Seung Han - all rights reserved
+        </p>
+      </div>
+    </footer>
   )
+}
+
+const style = {
+  footerWrapper: tw`
+    container
+    px-3
+    py-5
+    mx-auto
+    flex
+    items-center
+    sm:flex-row
+    flex-col
+    border-t-2
+    mt-4
+  `,
+  footerMainText: tw`
+    flex
+    font-medium
+    items-center
+    md:justify-start
+    justify-center
+    text-gray-900
+    ml-3
+    text-xl
+  `,
+  copyrightText: tw`
+    text-sm
+    text-gray-500
+    sm:ml-4
+    sm:pl-4
+    sm:border-l-2
+    sm:border-gray-200
+    sm:py-2
+    sm:mt-0
+    mt-4
+  `
 }
 
 export default Footer
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,59 @@
+import { css } from '@emotion/react'
 import React from 'react'
+import tw from 'twin.macro'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className="text-gray-600 body-font">
+      <div css={style.navbarWrapper}>
+        <a css={style.logoText} href="./" >bear109&apos;s portfolio</a>
+        <nav css={style.navTextWrapper}>
+          <a css={style.navText}>About Me</a>
+          <a css={style.navText}>Skills</a>
+          <a css={style.navText}>Project</a>
+          <a css={style.navText}>Contact Me</a>
+        </nav>
+      </div>
+    </header>
   )
+}
+
+
+const style = {
+  navbarWrapper: tw`
+    container
+    mx-auto
+    flex
+    flex-wrap
+    p-5
+    flex-col
+    md:flex-row
+    items-center
+  `,
+  logoText: tw`
+    flex
+    items-center
+    text-gray-900
+    mb-4
+    md:mb-0
+    ml-3 
+    text-2xl
+    font-extrabold
+  `,
+  navTextWrapper: tw`
+    ml-40
+    flex
+    flex-wrap
+    items-center
+    justify-between
+    w-3/5
+    text-lg
+    font-semibold
+  `,
+  navText: tw`
+    mr-5
+    hover:text-gray-900
+  `
 }
 
 export default Navbar
